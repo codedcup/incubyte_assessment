ef8231700a9d90072d881d820adfaafbeac1a941
codedcup <codedcup@gmail.com>
2024-06-02 15:27:00 +0530

    handling different delimiters

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    const nums = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        const cDelimiter = parts[0].slice(2);
+        delimiter = new RegExp(cDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+        numbers = parts[1];
+    }
+    const nums = numbers.split(delimiter);
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
 

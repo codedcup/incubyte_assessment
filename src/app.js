36c0c932e0c60f63de2fbb2e78ff2ed7d59996e3
codedcup <codedcup@gmail.com>
2024-06-02 14:40:28 +0530

    only one number test added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 function add(numbers) {
     if (numbers === "") {
-        return 0;
+      return 0;
+    }
+    if (numbers.length === 1) {
+      return parseInt(numbers);
     }
     return 0;
-}
+  }
 
 module.exports = add;

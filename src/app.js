325ebb14930c22609f1bc3826d8ddc9805c4a158
codedcup <codedcup@gmail.com>
2024-06-02 15:10:35 +0530

    modified sum of comma-separated numbers

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ function add(numbers) {
         return 0;
     }
     const nums = numbers.split(",");
-    if (nums.length === 1) {
-        return parseInt(numbers);
-    }
+    
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
